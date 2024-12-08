fb631309b0f1d3c496873ad35440fd89cbdb238b
bhalalaprushti30 <prushti.bhalala3010@gmail.com>
2024-12-08 12:41:37 +0530

    MVC

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,63 @@
+﻿using Calculator.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc()); // Initialize the model for the first GET request
+        }
+
+        [HttpPost]
+        public ActionResult Index(calc c, string calculate)
+        {
+            // Perform the calculation based on the button pressed
+            if (calculate == "add")
+            {
+                c.tot = c.no1 + c.no2;
+            }
+            else if (calculate == "min")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "mul")
+            {
+                c.tot = c.no1 * c.no2;
+            }
+            else if (calculate == "div")
+            {
+                if (c.no2 == 0)
+                {
+                    ModelState.AddModelError("", "Cannot divide by zero.");
+                    c.tot = 0;
+                }
+                else
+                {
+                    c.tot = c.no1 / c.no2;
+                }
+            }
+            else if (calculate == "mod")
+            {
+                if (c.no2 == 0)
+                {
+                    ModelState.AddModelError("", "Cannot mod by zero.");
+                    c.tot = 0;
+                }
+                else
+                {
+                    c.tot = c.no1 % c.no2;
+                }
+            }
+            else if (calculate == "exp")
+            {
+                c.tot = (int)Math.Pow(c.no1, c.no2);
+            }
+
+            return View(c); // Return the view with the model containing the result
+        }
+    }
+}
---
+++ Models/calc.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+{
+    public class calc
+    {
+        public double no1 { get; set; }
+
+        public double no2 { get; set; }
+
+        public double tot { get; set; }
+    }
+
+}
